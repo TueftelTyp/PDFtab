d3889c9e73141a4e96240ce3ee5deb38e9e39876
Tüftel Typ <31390221+TueftelTyp@users.noreply.github.com>
2026-03-27 15:56:53 +0100

    Add files via upload

---
+++ Source/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace PDFtab
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lvDoc = new System.Windows.Forms.ListView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pbStatus = new System.Windows.Forms.ToolStripProgressBar();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnExecuteCSV = new System.Windows.Forms.Button();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.chkOpen = new System.Windows.Forms.CheckBox();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.chkDestination = new System.Windows.Forms.CheckBox();
+            this.chkTitle = new System.Windows.Forms.CheckBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.chkFrame = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvDoc
+            // 
+            this.lvDoc.HideSelection = false;
+            this.lvDoc.Location = new System.Drawing.Point(12, 46);
+            this.lvDoc.Name = "lvDoc";
+            this.lvDoc.Size = new System.Drawing.Size(461, 333);
+            this.lvDoc.TabIndex = 0;
+            this.lvDoc.UseCompatibleStateImageBehavior = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus,
+            this.toolStripStatusLabel2,
+            this.pbStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 385);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(565, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(16, 17);
+            this.lblStatus.Text = "...";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(332, 17);
+            this.toolStripStatusLabel2.Spring = true;
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(200, 16);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 17);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnExecuteCSV
+            // 
+            this.btnExecuteCSV.Location = new System.Drawing.Point(479, 220);
+            this.btnExecuteCSV.Name = "btnExecuteCSV";
+            this.btnExecuteCSV.Size = new System.Drawing.Size(75, 23);
+            this.btnExecuteCSV.TabIndex = 3;
+            this.btnExecuteCSV.Text = "Convert";
+            this.btnExecuteCSV.UseVisualStyleBackColor = true;
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Enabled = false;
+            this.btnDestination.Location = new System.Drawing.Point(404, 17);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(69, 23);
+            this.btnDestination.TabIndex = 4;
+            this.btnDestination.Text = "Destination";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(479, 249);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // chkOpen
+            // 
+            this.chkOpen.AutoSize = true;
+            this.chkOpen.Location = new System.Drawing.Point(479, 128);
+            this.chkOpen.Name = "chkOpen";
+            this.chkOpen.Size = new System.Drawing.Size(74, 17);
+            this.chkOpen.TabIndex = 6;
+            this.chkOpen.Text = "auto open";
+            this.chkOpen.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(479, 298);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFolder.TabIndex = 7;
+            this.btnOpenFolder.Text = "Output";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(479, 356);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(75, 23);
+            this.btnLog.TabIndex = 8;
+            this.btnLog.Text = "log";
+            this.btnLog.UseVisualStyleBackColor = true;
+            // 
+            // chkDestination
+            // 
+            this.chkDestination.AutoSize = true;
+            this.chkDestination.Location = new System.Drawing.Point(346, 21);
+            this.chkDestination.Name = "chkDestination";
+            this.chkDestination.Size = new System.Drawing.Size(62, 17);
+            this.chkDestination.TabIndex = 9;
+            this.chkDestination.Text = "change";
+            this.chkDestination.UseVisualStyleBackColor = true;
+            // 
+            // chkTitle
+            // 
+            this.chkTitle.AutoSize = true;
+            this.chkTitle.Location = new System.Drawing.Point(479, 80);
+            this.chkTitle.Name = "chkTitle";
+            this.chkTitle.Size = new System.Drawing.Size(68, 17);
+            this.chkTitle.TabIndex = 10;
+            this.chkTitle.Text = "Headtitle";
+            this.chkTitle.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(479, 327);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // chkFrame
+            // 
+            this.chkFrame.AutoSize = true;
+            this.chkFrame.Location = new System.Drawing.Point(479, 57);
+            this.chkFrame.Name = "chkFrame";
+            this.chkFrame.Size = new System.Drawing.Size(45, 17);
+            this.chkFrame.TabIndex = 12;
+            this.chkFrame.Text = "Grid";
+            this.chkFrame.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 407);
+            this.Controls.Add(this.chkFrame);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.chkTitle);
+            this.Controls.Add(this.chkDestination);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.chkOpen);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.btnExecuteCSV);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lvDoc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDFtab";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvDoc;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripProgressBar pbStatus;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnExecuteCSV;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.CheckBox chkOpen;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.CheckBox chkDestination;
+        private System.Windows.Forms.CheckBox chkTitle;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.CheckBox chkFrame;
+    }
+}
+
---
+++ Source/Form1.cs
@@ -0,0 +1,443 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFtab
+{
+    public partial class MainForm : Form
+    {
+        private List<string> _documents = new List<string>();
+        private string _outputPath = "";
+        private CancellationTokenSource _cancellationTokenSource;
+        private readonly object _logLock = new object();
+        private readonly List<string> _logMessages = new List<string>();
+        private readonly string _logFilePath;
+        private int _processedCount = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            QuestPDF.Settings.License = LicenseType.Community;
+
+            _logFilePath = Path.Combine(Application.StartupPath, "PDFtab.log");
+
+            InitializeUI();
+            LoadLog();
+        }
+
+        private void InitializeUI()
+        {
+            btnLoad.Click += btnLoad_Click;
+            btnExecuteCSV.Click += btnExecuteCSV_Click;
+            btnCancel.Click += btnCancel_Click;
+            btnDestination.Click += btnDestination_Click;
+            btnOpenFolder.Click += btnOpenFolder_Click;
+            btnLog.Click += btnLog_Click;
+            btnClear.Click += btnClear_Click;
+
+            chkDestination.CheckedChanged += chkDestination_CheckedChanged;
+            chkFrame.CheckedChanged += chkFrame_CheckedChanged;  // Neu: Event für Checkbox
+
+            lvDoc.View = View.Details;
+            lvDoc.Columns.Add("Dateiname", 350);
+            lvDoc.Columns.Add("Status", 100);
+            lvDoc.FullRowSelect = true;
+            lvDoc.AllowDrop = true;
+
+            lvDoc.DragEnter += lvDoc_DragEnter;
+            lvDoc.DragDrop += lvDoc_DragDrop;
+
+            lblStatus.Text = "Bereit";
+            pbStatus.Value = 0;
+
+            btnDestination.Enabled = chkDestination.Checked;
+            btnCancel.Enabled = false;
+        }
+
+        // Neu: Event-Handler für chkFrame
+        private void chkFrame_CheckedChanged(object sender, EventArgs e)
+        {
+            // Optional: UI-Feedback
+            lblStatus.Text = chkFrame.Checked ? "Rahmen für Tabellen aktiviert" : "Rahmen deaktiviert";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            _documents.Clear();
+            lvDoc.Items.Clear();
+            _processedCount = 0;
+            UpdateProgress(0);
+            lblStatus.Text = "Liste geleert";
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "CSV Dateien (*.csv)|*.csv|Alle Dateien (*.*)|*.*";
+                dialog.Multiselect = true;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var path in dialog.FileNames)
+                    {
+                        if (Directory.Exists(path))
+                            LoadFolder(path);
+                        else if (File.Exists(path) &&
+                                 path.EndsWith(".csv", StringComparison.OrdinalIgnoreCase))
+                            AddDocument(path);
+                    }
+                }
+            }
+        }
+
+        private void LoadFolder(string folder)
+        {
+            var files = Directory.GetFiles(folder, "*.csv", SearchOption.AllDirectories);
+            foreach (var file in files)
+                AddDocument(file);
+
+            UpdateStatusSafe($"{files.Length} CSV Dateien aus Ordner geladen");
+        }
+
+        private void AddDocument(string file)
+        {
+            if (!_documents.Contains(file))
+            {
+                _documents.Add(file);
+                AddToListView(file, "Geladen");
+            }
+        }
+
+        private void lvDoc_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void lvDoc_DragDrop(object sender, DragEventArgs e)
+        {
+            var paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var path in paths)
+            {
+                if (Directory.Exists(path))
+                    LoadFolder(path);
+                else if (File.Exists(path) &&
+                         path.EndsWith(".csv", StringComparison.OrdinalIgnoreCase))
+                    AddDocument(path);
+            }
+        }
+
+        private void AddToListView(string filePath, string status)
+        {
+            var item = new ListViewItem(Path.GetFileName(filePath));
+            item.SubItems.Add(status);
+            item.Tag = filePath;
+            lvDoc.Items.Add(item);
+        }
+
+        private async void btnExecuteCSV_Click(object sender, EventArgs e)
+        {
+            if (_documents.Count == 0)
+            {
+                lblStatus.Text = "Keine CSV Dateien geladen!";
+                return;
+            }
+
+            _processedCount = 0;
+            UpdateProgress(0);
+
+            btnExecuteCSV.Enabled = false;
+            btnCancel.Enabled = true;
+
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                await ConvertAllCSVsAsync();
+            }
+            catch (OperationCanceledException)
+            {
+                UpdateStatusSafe("Abgebrochen");
+            }
+            finally
+            {
+                btnExecuteCSV.Enabled = true;
+                btnCancel.Enabled = false;
+
+                if (chkOpen.Checked)
+                    OpenOutputFolder();
+            }
+        }
+
+        private async Task ConvertAllCSVsAsync()
+        {
+            int total = _documents.Count;
+
+            await Task.Run(() =>
+            {
+                Parallel.ForEach(_documents, new ParallelOptions
+                {
+                    CancellationToken = _cancellationTokenSource.Token,
+                    MaxDegreeOfParallelism = Environment.ProcessorCount
+                },
+                csvFile =>
+                {
+                    try
+                    {
+                        _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+
+                        UpdateListViewStatus(csvFile, "Konvertiere...");
+
+                        string outputFile = GetOutputFilePath(csvFile);
+                        Directory.CreateDirectory(Path.GetDirectoryName(outputFile));
+
+                        ConvertCSVToPDF(csvFile, outputFile);
+
+                        int done = Interlocked.Increment(ref _processedCount);
+
+                        UpdateListViewStatus(csvFile, "✓ Fertig");
+                        LogMessage($"✓ {Path.GetFileName(csvFile)}");
+
+                        int percent = (int)((done * 100.0) / total);
+                        UpdateProgress(percent);
+                        UpdateStatusSafe($"Fortschritt: {done}/{total}");
+                    }
+                    catch (Exception ex)
+                    {
+                        UpdateListViewStatus(csvFile, "✗ Fehler");
+                        LogMessage($"✗ {csvFile}: {ex.Message}");
+                    }
+                });
+            });
+
+            UpdateProgress(100);
+            UpdateStatusSafe("Fertig");
+        }
+
+        private List<string[]> ParseCsv(string file)
+        {
+            var result = new List<string[]>();
+            foreach (var line in File.ReadLines(file))
+            {
+                var row = new List<string>();
+                bool inQuotes = false;
+                string current = "";
+                foreach (char c in line)
+                {
+                    if (c == '"')
+                        inQuotes = !inQuotes;
+                    else if ((c == ';' || c == ',') && !inQuotes)
+                    {
+                        row.Add(current);
+                        current = "";
+                    }
+                    else
+                        current += c;
+                }
+                row.Add(current);
+                result.Add(row.ToArray());
+            }
+            return result;
+        }
+
+        private void ConvertCSVToPDF(string csvFile, string pdfFile)
+        {
+            var rows = ParseCsv(csvFile);
+            bool drawFrame = chkFrame.Checked;
+
+            Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(20);
+
+                    if (chkTitle.Checked)
+                    {
+                        page.Header()
+                            .Container()
+                            .Padding(10f)
+                            .AlignCenter()
+                            .Text(Path.GetFileNameWithoutExtension(csvFile))
+                            .Bold()
+                            .FontSize(16);
+                    }
+
+                    page.Content().Table(table =>
+                    {
+                        int columnCount = rows.Max(r => r.Length);
+
+                        table.ColumnsDefinition(columns =>
+                        {
+                            for (int i = 0; i < columnCount; i++)
+                                columns.RelativeColumn(1u); // uint statt int
+                        });
+
+                        if (rows.Count > 0)
+                        {
+                            // Header mit Rahmen und Farbe, wenn aktiviert
+                            table.Header(header =>
+                            {
+                                foreach (var cellText in rows[0])
+                                {
+                                    header.Cell().Container()
+                                        .Padding(4f)
+                                        .Border(drawFrame ? 1f : 0f)
+                                        .BorderColor(drawFrame ? Colors.Grey.Medium : Colors.Transparent)
+                                        .Background(drawFrame ? Colors.Grey.Lighten4 : Colors.Transparent)
+                                        .Text(cellText)
+                                        .SemiBold()
+                                        .FontSize(11);
+                                }
+                            });
+
+                            // Datenzeilen mit feinen Trennlinien
+                            for (int i = 1; i < rows.Count; i++)
+                            {
+                                var rowIndex = i; // Für Closure
+                                foreach (var cellText in rows[i])
+                                {
+                                    table.Cell().Container()
+                                        .Padding(4f)
+                                        .BorderBottom(drawFrame ? 0.5f : 0f)
+                                        .BorderRight(drawFrame ? 0.5f : 0f)
+                                        .BorderColor(drawFrame ? Colors.Grey.Lighten2 : Colors.Transparent)
+                                        .Text(cellText)
+                                        .FontSize(10);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            table.Cell().ColumnSpan((uint)columnCount)
+                                .AlignCenter()
+                                .Text("Keine Daten gefunden");
+                        }
+                    });
+
+                    page.Footer().AlignCenter().Text(x =>
+                    {
+                        x.CurrentPageNumber();
+                        x.Span(" / ");
+                        x.TotalPages();
+                    });
+                });
+            }).GeneratePdf(pdfFile);
+        }
+
+        private string GetOutputFilePath(string csvFile)
+        {
+            string dir = chkDestination.Checked && !string.IsNullOrEmpty(_outputPath)
+                ? _outputPath
+                : Path.GetDirectoryName(csvFile);
+
+            return Path.Combine(dir, Path.GetFileNameWithoutExtension(csvFile) + ".pdf");
+        }
+
+        private string GetOutputFolder()
+        {
+            return chkDestination.Checked && !string.IsNullOrEmpty(_outputPath)
+                ? _outputPath
+                : Path.GetDirectoryName(_documents.FirstOrDefault() ?? "");
+        }
+
+        private void UpdateListViewStatus(string filePath, string status)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => UpdateListViewStatus(filePath, status)));
+                return;
+            }
+
+            foreach (ListViewItem item in lvDoc.Items)
+            {
+                if ((string)item.Tag == filePath)
+                    item.SubItems[1].Text = status;
+            }
+        }
+
+        private void UpdateProgress(int value)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => UpdateProgress(value)));
+                return;
+            }
+
+            pbStatus.Value = Math.Max(0, Math.Min(100, value));
+        }
+
+        private void UpdateStatusSafe(string text)
+        {
+            if (InvokeRequired)
+                Invoke(new Action(() => lblStatus.Text = text));
+            else
+                lblStatus.Text = text;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            _cancellationTokenSource?.Cancel();
+            lblStatus.Text = "Abbruch...";
+        }
+
+        private void chkDestination_CheckedChanged(object sender, EventArgs e)
+        {
+            btnDestination.Enabled = chkDestination.Checked;
+        }
+
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    _outputPath = fbd.SelectedPath;
+                    lblStatus.Text = $"Ziel: {Path.GetFileName(_outputPath)}";
+                }
+            }
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            OpenOutputFolder();
+        }
+
+        private void OpenOutputFolder()
+        {
+            string folder = GetOutputFolder();
+            if (Directory.Exists(folder))
+                Process.Start("explorer.exe", folder);
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", _logFilePath);
+        }
+
+        private void LogMessage(string message)
+        {
+            lock (_logLock)
+            {
+                string entry = $"{DateTime.Now:HH:mm:ss} - {message}";
+                _logMessages.Add(entry);
+                File.WriteAllLines(_logFilePath, _logMessages);
+            }
+        }
+
+        private void LoadLog()
+        {
+            if (File.Exists(_logFilePath))
+                _logMessages.AddRange(File.ReadAllLines(_logFilePath));
+        }
+    }
+}
